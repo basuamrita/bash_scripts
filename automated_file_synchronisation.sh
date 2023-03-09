c6fa6fa3293b77187ab321ddaf9663ea4c5ebfb6
AmritaBasu <basuamrita18@gmail.com>
2023-03-09 14:23:06 +0530

    committed

---
+++ automated_file_synchronisation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set the source and destination directories
+SOURCE_DIR=/path/to/source/directory
+DEST_DIR=/path/to/destination/directory
+
+# Sync the files
+rsync -avz $SOURCE_DIR/ $DEST_DIR/
