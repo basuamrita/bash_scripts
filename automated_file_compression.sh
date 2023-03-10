01b06dee4e5c98da4d064b4cfe3327b1efde1602
AmritaBasu <basuamrita18@gmail.com>
2023-03-10 20:48:42 +0530

    VERSION2

--- automated_file_compression.sh
+++ automated_file_compression.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # Set the directory containing the files to compress
-DIR=logs_archive
+DIR="/mnt/c/Users/Rajdeep/OneDrive/Desktop"
 
 # Set the compression format
+FORMAT=tar.gz
 
 # Compress the files
-tar -czvf logs_archive.tar.gz *
+tar -czvf $DIR/files.$FORMAT $DIR/*
