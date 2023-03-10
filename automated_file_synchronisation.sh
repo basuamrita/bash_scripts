0c30102204bb0401d1e5ccbbc2b1d70e1dd124a4
AmritaBasu <basuamrita18@gmail.com>
2023-03-10 23:36:02 +0530

    version2

--- automated_file_synchronisation.sh
+++ automated_file_synchronisation.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Set the source and destination directories
-SOURCE_DIR=/path/to/source/directory
-DEST_DIR=/path/to/destination/directory
+SOURCE_DIR="/mnt/c/Users/Rajdeep/OneDrive/Desktop/GITHUB"
+DEST_DIR="/mnt/c/Users/Rajdeep/OneDrive/Desktop/GITHUB/dump"
 
 # Sync the files
 rsync -avz $SOURCE_DIR/ $DEST_DIR/
