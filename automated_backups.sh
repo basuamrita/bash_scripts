b650360e707ba85dd641b773c85a8fcb38dc9303
AmritaBasu <basuamrita18@gmail.com>
2023-03-10 14:17:46 +0530

    version2

--- automated_backups.sh
+++ automated_backups.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Set the backup location
-BACKUP_DIR=(/home/amrita)
+BACKUP_DIR="/mnt/c/Users/Rajdeep/OneDrive/Desktop"
 
 # Set the source directory to backup
-SOURCE_DIR=(/mnt/c/Users/Rajdeep/OneDrive/Desktop/GITHUB)
+SOURCE_DIR="/mnt/c/Users/Rajdeep/OneDrive/Desktop/GITHUB"
 
 # Set the backup filename
 BACKUP_FILENAME=backup_$(date +%Y%m%d).tar.gz
 
 # Create the backup
-tar -cvzf $BACKUP_FILENAME *
-
+#tar -cvzf $BACKUP_FILENAME *
+tar -czf $BACKUP_DIR/$BACKUP_FILENAME -P $SOURCE_DIR
