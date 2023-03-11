d45a6f1348a437be5dfdf168f2ba2200202062b4
AmritaBasu <basuamrita18@gmail.com>
2023-03-11 13:16:24 +0530

    version2

--- automated_file_deletion.sh
+++ automated_file_deletion.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Set the directory containing the files to delete
-DIR=/path/to/directory
+DIR=/mnt/c/Users/Rajdeep/OneDrive/Desktop/code_linux
 
 # Delete files older than 7 days
 find $DIR -type f -mtime +7 -delete
