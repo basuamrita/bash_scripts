1404320588fa15524cca1b2ef7247dd6dfe5bb20
AmritaBasu <basuamrita18@gmail.com>
2023-03-11 13:03:36 +0530

    version2

--- automated_file_conversion.sh
+++ automated_file_conversion.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Set the directory containing the files to convert
-DIR=/path/to/directory
+DIR="/mnt/c/Users/Rajdeep/OneDrive/Desktop/testing_songs"
 
 # Convert the files from mp3 to wav
-for file in $DIR/*.mp3; do
-  ffmpeg -i "$file" -acodec pcm_s16le -ac 1 -ar 44100 "${file%.mp3}.wav"
+for file in $DIR/*.mp3;
+ do
+ ffmpeg -i "$file" -acodec pcm_s16le -ac 1 -ar 44100 "${file%.mp3}.wav"
 done
+# ffmpeg -i Lone_Ranger.mp4 Lone_Ranger.avi
