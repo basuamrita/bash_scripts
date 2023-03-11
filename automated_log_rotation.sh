ececa2cf21d66abde7a7608c21b7119abda7a435
AmritaBasu <basuamrita18@gmail.com>
2023-03-11 23:30:57 +0530

    version2

--- automated_log_rotation.sh
+++ automated_log_rotation.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
-    
+#!/usr/bin/env bash
+
 # Set the log file to rotate
-LOG_FILE=/path/to/logfile.log
+LOG_FILE="/mnt/c/Users/Rajdeep/OneDrive/Desktop/var/log"
 
 # Set the maximum size for the log file (in bytes)
 MAX_LOG_SIZE=1048576
 
 # Rotate the log file
-if [ $(stat -c%s $LOG_FILE) -gt $MAX_LOG_SIZE ]; then
-  mv $LOG_FILE $LOG_FILE.$(date +%Y%m%d)
+if [ $(stat -c%s $LOG_FILE) -gt $MAX_LOG_SIZE ] 
+then
+  mv $LOG_FILE $LOG_FILE.$(date +"%Y-%m-%d")
   touch $LOG_FILE
 fi
