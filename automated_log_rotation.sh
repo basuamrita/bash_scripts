2b4f0146713460c2ca4cf7c7fc579f6b5d678a8b
ppdsocio <116940886+ppdsocio@users.noreply.github.com>
2023-03-12 00:00:42 +0530

    Error with Double quotation
     Issue at MAX_LOG_SIZE="1048576"

--- automated_log_rotation.sh
+++ automated_log_rotation.sh
@@ -4,7 +4,7 @@
 LOG_FILE="/mnt/c/Users/Rajdeep/OneDrive/Desktop/var/log"
 
 # Set the maximum size for the log file (in bytes)
-MAX_LOG_SIZE=1048576
+MAX_LOG_SIZE="1048576"
 
 # Rotate the log file
 if [ $(stat -c%s $LOG_FILE) -gt $MAX_LOG_SIZE ] 
